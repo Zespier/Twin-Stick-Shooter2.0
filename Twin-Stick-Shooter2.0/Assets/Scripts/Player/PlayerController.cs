3674790f4b3de75e139a553995a64dc3d608a2fa
Zespier <pabloreco.redondo@gmail.com>
2024-03-13 00:29:21 +0100

    More assets

--- Twin-Stick-Shooter2.0/Assets/Scripts/Player/PlayerController.cs
+++ Twin-Stick-Shooter2.0/Assets/Scripts/Player/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour {
 
     public float speed = 9f;
     public Transform body;
-    //public PlayerInputs playerInputs;
+    public PlayerInputs playerInputs;
 
     [HideInInspector] public Vector2 _moveValue;
     [HideInInspector] public Vector2 _moveDirectionLerped;
@@ -18,15 +18,15 @@ public class PlayerController : MonoBehaviour {
             instance = this;
         }
 
-        //playerInputs = new PlayerInputs();
+        playerInputs = new PlayerInputs();
     }
 
     private void OnEnable() {
-        //playerInputs.Enable();
+        playerInputs.Enable();
     }
 
     private void OnDisable() {
-        //playerInputs.Disable();
+        playerInputs.Disable();
     }
 
     private void Update() {
@@ -38,7 +38,7 @@ public class PlayerController : MonoBehaviour {
     /// Reads the player inputs on the left joystick to move
     /// </summary>
     private void Movement() {
-        //_moveValue = playerInputs.Player.Move.ReadValue<Vector2>();
+        _moveValue = playerInputs.Player.Move.ReadValue<Vector2>();
 
         _moveDirectionLerped = Vector2.Lerp(_moveDirectionLerped, _moveValue, Time.deltaTime / 0.1f);
         transform.position += Time.deltaTime * speed * new Vector3(_moveDirectionLerped.x, _moveDirectionLerped.y, 0);
@@ -48,20 +48,20 @@ public class PlayerController : MonoBehaviour {
     /// Reads the player inputs on the right joystick to rotate
     /// </summary>
     private void Rotation() {
-        //Vector2 lookValue = GetLookValue();
+        Vector2 lookValue = GetLookValue();
 
-        //body.up = Vector3.Lerp(body.up, lookValue, Time.deltaTime / 0.03f);
+        body.up = Vector3.Lerp(body.up, lookValue, Time.deltaTime / 0.03f);
 
-        //Events.OnTargetMove?.Invoke(lookValue.normalized);
+        Events.OnTargetMove?.Invoke(lookValue.normalized);
     }
 
-    //private Vector2 GetLookValue() {
-    //    //Vector2 lookValue = playerInputs.Player.Look.ReadValue<Vector2>();
+    private Vector2 GetLookValue() {
+        Vector2 lookValue = playerInputs.Player.Look.ReadValue<Vector2>();
 
-    //    if (lookValue == Vector2.zero) {
-    //        lookValue = _moveValue;
-    //    }
+        if (lookValue == Vector2.zero) {
+            lookValue = _moveValue;
+        }
 
-    //    return lookValue;
-    //}
+        return lookValue;
+    }
 }
